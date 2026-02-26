cc073122b8b9c3187d029fcdbb027c9de530fc3f
KeeRReeK <keerreekdev@gmail.com>
2026-02-26 12:18:43 +0200

    chore: initial commit

---
+++ WeatherNotes/WeatherNotes/App/WeatherNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNotesApp.swift
+//  WeatherNotes
+//
+//  Created by KeeR ReeK on 26.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
