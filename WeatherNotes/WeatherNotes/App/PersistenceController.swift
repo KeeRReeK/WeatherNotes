3e1604bba9d3a8e431704fba78cf4c9bc6b61114
KeeRReeK <keerreekdev@gmail.com>
2026-02-28 03:10:14 +0200

    chore: add PersistenceController

---
+++ WeatherNotes/WeatherNotes/App/PersistenceController.swift
@@ -0,0 +1,27 @@
+//
+//  PersistenceController.swift
+//  WeatherNotes
+//
+//  Created by KeeR ReeK on 28.02.2026.
+//
+
+import CoreData
+
+struct PersistenceController {
+    static let shared = PersistenceController()
+
+    let container: NSPersistentContainer
+
+    init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "WeatherNotes")
+        if inMemory {
+            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        }
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+}
